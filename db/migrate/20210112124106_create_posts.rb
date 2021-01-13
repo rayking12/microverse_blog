3964d9ad07915faa99ff5c31cee4f413bfb9bd3e
helciodev <helcioandre@live.com>
2021-01-13 13:48:11 +0100

    reverts db to original

--- db/migrate/20210112124106_create_posts.rb
+++ db/migrate/20210112124106_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-      t.string :author
+      t.string :authour
       t.string :title
       t.text :content
 
