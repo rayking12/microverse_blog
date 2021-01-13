3c778b2da653698da64041ddaff1ec3d143ae9c7
rayking12 <rayodo667@gmail.com>
2021-01-13 14:48:20 +0100

    added index and show controller method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,25 @@
 class PostsController < ApplicationController
-  def index
-        
-  end
-  def new
+    def index
+        @posts = Post.all.order("created_at DESC")
+    end
+    def new
         @post = Post.new
-  end
+    end
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to @post
+        else 
+            render 'new'
+        end
+    end
+    def show
+        @post = Post.find(params[:id])
+    end
 
-  private
-  def params
-        @post.find()
-  end
-
-  def post_params
-        params.require(:post).permit(:author, :title, :content)
-  end
+    private 
+    def post_params 
+        params.require(:post).permit(:authour, :title, :content) 
+    end
 end
+
